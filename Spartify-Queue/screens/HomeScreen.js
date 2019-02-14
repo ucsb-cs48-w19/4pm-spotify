a16e2ccff8c15dc515877c8a1f29e189ddceabcd
Kyle Giannini <kylgia@gmail.com>
2019-02-14 00:04:52 -0800

    Fallback to this until you get Play URI working.

---
+++ Spartify-Queue/screens/HomeScreen.js
@@ -0,0 +1,139 @@
+import React, { Component } from 'react';
+import { TouchableOpacity, StyleSheet, Text, View, Image, AsyncStorage } from 'react-native';
+import { AuthSession } from 'expo';
+import { FontAwesome } from '@expo/vector-icons';
+import axios from 'axios';
+
+const CLIENT_ID = '1b256be8537d49249f3785fd1c05012c';
+
+export default class App extends Component {
+  state = {
+    userInfo: null,
+    didError: false
+  };
+
+  handleSpotifyLogin = async () => {
+    let redirectUrl = AuthSession.getRedirectUrl();
+    let results = await AuthSession.startAsync({
+      authUrl: `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(redirectUrl)}&scope=user-read-email&response_type=token`
+    });
+    if (results.type !== 'success') {
+      console.log(results.type);
+      this.setState({ didError: true });
+    } else {
+      const userInfo = await axios.get(`https://api.spotify.com/v1/me`, {
+        headers: {
+          "Authorization": `Bearer ${results.params.access_token}`
+        }
+      });
+      try {
+        await AsyncStorage.setItem('Authorization', `Bearer ${results.params.access_token}`);
+      } catch (error) {
+        // Error saving data
+      }
+      this.setState({ userInfo: userInfo.data });
+    }
+  };
+
+  displayError = () => {
+    return (
+      <View style={styles.userInfo}>
+        <Text style={styles.errorText}>
+          There was an error, please try again.
+        </Text>
+      </View>
+    );
+  }
+
+  displayResults = () => {
+    { return this.state.userInfo ? (
+      <View style={styles.userInfo}>
+        <Image
+          style={styles.profileImage}
+          source={ {'uri': this.state.userInfo.images[0].url} }
+        />
+        <View>
+          <Text style={styles.userInfoText}>
+            Username:
+          </Text>
+          <Text style={styles.userInfoText}>
+            {this.state.userInfo.id}
+          </Text>
+          <Text style={styles.userInfoText}>
+            Email:
+          </Text>
+          <Text style={styles.userInfoText}>
+            {this.state.userInfo.email}
+          </Text>
+        </View>
+      </View>
+    ) : (
+      <View style={styles.userInfo}>
+        <Text style={styles.userInfoText}>
+          Login to Spotify to see user data.
+        </Text>
+      </View>
+    )}
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <FontAwesome
+          name="spotify"
+          color="#2FD566"
+          size={128}
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={this.handleSpotifyLogin}
+          disabled={this.state.userInfo ? true : false}
+        >
+          <Text style={styles.buttonText}>
+            Login with Spotify
+          </Text>
+        </TouchableOpacity>
+        {this.state.didError ?
+          this.displayError() :
+          this.displayResults()
+        }
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    backgroundColor: '#000',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  button: {
+    backgroundColor: '#2FD566',
+    padding: 20
+  },
+  buttonText: {
+    color: '#000',
+    fontSize: 20
+  },
+  userInfo: {
+    height: 250,
+    width: 200,
+    alignItems: 'center',
+  },
+  userInfoText: {
+    color: '#fff',
+    fontSize: 18
+  },
+  errorText: {
+    color: '#fff',
+    fontSize: 18
+  },
+  profileImage: {
+    height: 64,
+    width: 64,
+    marginBottom: 32
+  }
+});
