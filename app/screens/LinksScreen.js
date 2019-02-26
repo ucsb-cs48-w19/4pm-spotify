245593ee490bc32f1b9aa11d303028e3538347d2
maxcolbert <mcolbertmp@gmail.com>
2019-02-25 20:16:16 -0800

    search + display
    added search functionality for Spotify API, with toggle between searching and queue display. plus extras

--- app/screens/LinksScreen.js
+++ app/screens/LinksScreen.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Image, Platform, ScrollView, StyleSheet, Text, TouchableOpacity, View, FlatList, TouchableHighlight, AsyncStorage } from 'react-native';
+import { Image, Button, Platform, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View, FlatList, TouchableHighlight, AsyncStorage } from 'react-native';
 import { WebBrowser, AuthSession } from 'expo';
 // import { WebBrowser, AuthSession } from 'expo';
 import { FontAwesome } from '@expo/vector-icons';
 import axios from 'axios';  // 'npm install axios --save'
 import { MonoText } from '../components/StyledText';
+// https://stackoverflow.com/questions/52055391/onsubmitediting-never-fires
 
-const CLIENT_ID = '1b256be8537d49249f3785fd1c05012c';
+// const CLIENT_ID = '09b7b0f745014fb0950ee5bf040fbe3a';
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
@@ -14,7 +15,23 @@ export default class HomeScreen extends React.Component {
   };
 
   state = {
-    devices: []
+    devices: [],
+    // USING SAMPLE DATA, must pull from DB:
+    // queueSongs: [],
+    queueSongs: [
+              {name: 'ZEZE', artist: 'Travis', uri: 'spotify:track:7l3E7lcozEodtVsSTCkcaA'},
+              {name: 'Love it', artist: 'Kanye', uri: 'spotify:track:4S8d14HvHb70ImctNgVzQQ'},
+              {name: 'Digits', artist: 'Thug', uri: 'spotify:track:4cg1yakyRSIOjxKM2I7J1q'},
+              {name: 'FML', artist: 'Kanye', uri: 'spotify:track:34kRg5EbCB3r20QXZbnGeY'},
+              {name: 'Champions', artist: 'Queen', uri: 'spotify:track:7ccI9cStQbQdystvc6TvxD'}
+            ],
+    searchData: [],
+    search : '',
+    showSearch: false
+  }
+
+  toggleShowSearch = async (value) => {
+      this.setState({ showSearch: value });
   }
 
   getDevice = async (uri) => {
@@ -44,6 +61,41 @@ export default class HomeScreen extends React.Component {
     };
   }
 
+  searchShit = async (queryData) => {
+    try {
+      const value = await AsyncStorage.getItem('Authorization');
+      if (value !== null) {
+        var q = 'abba'; var type = 'track'; var market = 'US'; // "type": 'album,track,artist,playlist'
+        await axios.get(`https://api.spotify.com/v1/search?q=`+q+`&type=`+type+`&market=`+market,
+          {
+            headers: {
+              authorization: value,
+              'Content-Type': 'application/json'
+            },
+            // data: JSON.stringify({  "q": 'abba', "type": 'track', "market": 'US'  }),
+          })
+          .then(response => {
+            // console.log("response: \n", response.data);
+            this.state.searchData = [];
+            for(i = 0; i < response.data.tracks.items.length; i++) {
+              this.state.searchData.push( { "name": response.data.tracks.items[i].name, "artist": response.data.tracks.items[i].artists[0].name, "uri": response.data.tracks.items[i].uri } );
+              // dataArray[response.data.tracks.items[i]] = [response.data.tracks.items[i].name, response.data.tracks.items[i].artists[0].name, response.data.tracks.items[i].uri];
+            }
+            // this.setState({ searchedData: dataArray });
+            console.log(this.state.searchData);
+
+            // https://stackoverflow.com/questions/30626030/can-you-force-a-react-component-to-rerender-without-calling-setstate
+            // https://reactjs.org/docs/react-component.html#forceupdate
+            this.forceUpdate()
+          })
+
+      }
+    } catch (error) {
+      console.log("Error retrieving data: ", error);
+      // Error retrieving data
+    };
+  };
+
   playSong = async (device_id, uri) => {
     try {
       const value = await AsyncStorage.getItem('Authorization');
@@ -69,80 +121,88 @@ export default class HomeScreen extends React.Component {
           // .then (function (response) {console.log( response.json() ); return response.json()})
           .then(function (json) {console.log(json)/* Here is your json */})
           // .catch(function (error) {console.log(error)/*Handle error*/});
-
-          // .then((response) => response.json())
-          // .then((responseData) => {
-          //   return fetch(/*api.searchservice + */responseData)
-          //     .then((response) => response.json())
-          //     .then((responseData) => {
-          //       console.log(responseData);
-          //     })
-          // })
-          // .catch(function (error) {
-          //   console.log(error);
-          //   return error;
-          // });
       }
     } catch (error) {
         console.log("Error retrieving data: ", error);
       // Error retrieving data
     };
-
-    // $.ajax({
-    //    url: "https://api.spotify.com/v1/me/player/play?", //device_id=" + device_id,
-    //    type: "PUT",
-    //    data: '{"uris": ["spotify:track:5ya2gsaIhTkAuWYEMB0nw5"]}',
-    //    beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer ' + _token );},
-    //    success: function(data) { 
-    //      console.log(data)
-    //    }
-    // });
   }
+
+  onSearch = async (val) => {
+    let searchText = val.nativeEvent.text;
+    this.setState({ search: searchText });
+    console.log(this.state.search);
+    this.searchShit(this.state.search);
+  }
+  showSearch = async () => {
+    this.toggleShowSearch(true);
+  }
+  cancelSearch = async () => {
+    this.toggleShowSearch(false);
+    this.textInput.clear();
+    this.state.searchData = [];
+    // https://stackoverflow.com/questions/30626030/can-you-force-a-react-component-to-rerender-without-calling-setstate
+    // https://reactjs.org/docs/react-component.html#forceupdate
+    this.forceUpdate()
+  }
+
+  sendSongToQueue = async (song) => {
+    console.log(song, "\n", song.name, "\n", song.artist, "\n", song.uri);
+  }
+
+  getQueueSongs = async () => {
+    console.log("retrieving songs...");
+    var songs = []; // GET SONGS HERE
+    this.setState({ queueSongs: songs });
+  }
+
+  // OUR OLD PLAY CALL, this call should be used if you are host of playlist, not otherwise:
+  // renderItem={({item}) => {
+  //     return(
+  //       <TouchableHighlight onPress={() => this.getDevice(item.key)}>
+  //            <Text style={styles.item}>{item.name}</Text>
+  //       </TouchableHighlight>
+  //     )
+  //   }
+  // }
 
   render() {
     return (
       <ScrollView>
       <View style={styles.container}>
-        <FlatList
-          data={[
-            {name: 'ZEZE', key: 'spotify:track:7l3E7lcozEodtVsSTCkcaA'},
-            {name: 'Love it', key: 'spotify:track:4S8d14HvHb70ImctNgVzQQ'},
-            {name: 'Digits', key: 'spotify:track:4cg1yakyRSIOjxKM2I7J1q'},
-            {name: 'FML', key: 'spotify:track:34kRg5EbCB3r20QXZbnGeY'},
-            {name: 'Champions', key: 'spotify:track:7ccI9cStQbQdystvc6TvxD'},
-          ]}
-          // renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
-          renderItem={({item}) => {
+        <View style={{flexDirection:'row', backgroundColor:"white"}}>
+          <TextInput
+            placeholder="Search Spotify"
+            onSubmitEditing={this.onSearch}
+            onFocus={this.showSearch}
+            // onCancel={this.cancelSearch}
+            // onBlur={this.cancelSearch}
+            returnKeyLabel="Search"
+            returnKeyType="search"
+            ref={input => { this.textInput = input }}
+            style={{ width: this.state.showSearch ? '80%' : '100%', textAlign: 'center', padding: 10 }}
+          />
+          <TouchableHighlight hide={this.state.showSearch ? false : true} onPress={() => this.cancelSearch()}>
+            <Text style={{ width: '100%', textAlign: 'center', padding: 10, fontSize: 10 }}>Cancel</Text>
+          </TouchableHighlight>
+        </View>
+
+        <View style={{backgroundColor:"white"}}>
+          <FlatList
+            data={this.state.showSearch ? this.state.searchData : this.state.queueSongs}
+            extraData={this.state.showSearch ? this.state.searchData : this.state.queueSongs}
+            keyExtractor={ (item, index) => index }
+            renderItem={({item, index}) => {
               return(
-                <TouchableHighlight onPress={() => this.getDevice(item.key)}>
-                     <Text style={styles.item}>{item.name}</Text>
+                <TouchableHighlight onPress={() => this.state.showSearch ? this.sendSongToQueue(item) : null}>
+                  <Text style={styles.item}>{item.name}</Text>
                 </TouchableHighlight>
               )
-            }
-          }
-        />
+            }}
+          />
+        </View>
       </View>
       </ScrollView>
-      // <View style={styles.container}>
-      //   <FontAwesome
-      //     name="spotify"
-      //     color="#2FD566"
-      //     size={128}
-      //   />
-      //   <TouchableOpacity
-      //     style={styles.button}
-      //     onPress={this.handleSpotifyLogin}
-      //     disabled={this.state.userInfo ? true : false}
-      //   >
-      //     <Text style={styles.buttonText}>
-      //       Login with Spotify
-      //     </Text>
-      //   </TouchableOpacity>
-      //   {this.state.didError ?
-      //     this.displayError() :
-      //     this.displayResults()
-      //   }
-      // </View>
     );
   }
 
@@ -169,7 +229,7 @@ const styles = StyleSheet.create({
   },
   item: {
     padding: 10,
-    fontSize: 18,
+    fontSize: 15,
     height: 44,
     // color: '#1DB954',
   },
